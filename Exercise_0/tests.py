42b7d1b785c9eee5f405f86237044c7d7aefaff7
Tobias Famos <tobias.famos@outlook.com>
2023-02-24 12:12:16 +0100

    Write tests for part 1

--- Exercise_0/tests.py
+++ Exercise_0/tests.py
@@ -1,6 +1,17 @@
 import unittest
 import utils
 import networkx as nx
+import ex0
+import os
+
+def emtpy_drawings_folder():
+
+    for filename in os.listdir("Exercise_0/drawings"):        
+        os.remove(filename)
+
+def is_directory_empty(path):  
+    dir = os.listdir(path)
+    return len(dir) == 0
 
 class ReadingGraphs(unittest.TestCase):
     def test_does_return_something(self):
@@ -10,6 +21,21 @@ class ReadingGraphs(unittest.TestCase):
     def test_is_nx_graph(self):
         graph = utils.load_graph("Exercise_0/graphs/graph_00.graphml")
         self.assertIs(type(graph), nx.Graph)
+
+    def test_part_1_returns_list(self):
+        result = ex0.part1()
+        self.assertIs(type(result), list)
+
+    def test_part_1_returns_list_of_graphs(self):
+        results = ex0.part1()
+        for result in results:
+            self.assertIs(type(result), nx.Graph) 
+
+    def test_part_1_plots_graphs(self):
+        emtpy_drawings_folder()
+        resutls = ex0.part1()
+        self.assertTrue(is_directory_empty("Exercise_0/drawings"))
+
 
 
 
