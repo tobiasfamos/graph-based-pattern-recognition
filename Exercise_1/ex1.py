8d439214d487b4f79f1485c3e348227189f0d6f9
Brian6330 <games@schweigler.org>
2023-03-11 17:25:46 +0100

    1: WIP Implementation of recursive part

--- Exercise_1/ex1.py
+++ Exercise_1/ex1.py
@@ -17,15 +17,28 @@ from utils import load_graph, load_all_graphs, draw_graph, draw_all_graphs
 # You can potentially add more code here (constants, functions, ...)
 
 
-def _ullman_recursive() -> bool:
+def _ullman_recursive(mat: np.mat, g1: nx.Graph, g2: nx.Graph) -> bool:
     """
     Recursive part of the Ullman's algorithm
-    # TODO Is this the pruning part?
+
+    Args:
+        mat: Initialized Ullman matrix
+        g1: A networkx graph object
+        g2: A networkx graph object
+
     Returns:
         True if g1 is a subgraph of g2 and False otherwise
     """
     # Code here
-    return False
+    for w in range(1, g1.number_of_nodes()):  # Line 3 in Alg 2
+        for w2 in range(1, g2.number_of_nodes()):  # Line 4 in Alg 2
+            # TODO The following line, what exactly is u, v, or rather A in Serie_1.pdf
+            # if mat[u, w2 - 1] != mat[v, w2 - 1]:  # Line 5 in Alg 2 TODO!
+                    mat[w - 1, w2 - 1] = 0  # Line 6 in Alg 2
+        for row in range(0, mat.shape[0]):  # Line 8 in Alg 2
+            if mat[row, :].all() == 0:  # Line 8 in Alg 2
+                return False  # Line 9 in Alg 2
+    return True
 
 
 def Ullman(g1: nx.Graph, g2: nx.Graph) -> bool:
@@ -40,15 +53,16 @@ def Ullman(g1: nx.Graph, g2: nx.Graph) -> bool:
         True if g1 is a subgraph of g2 and False otherwise
     """
     ullman_mat = np.zeros((g1.number_of_nodes(), g2.number_of_nodes()))
-    for s in range(1, g1.number_of_nodes()):
-        for t in range(1, g2.number_of_nodes()):
-            if len(g1.edges(g1.nodes[s])) <= len(g2.edges(g2.nodes[t])):
-                ullman_mat[s-1, t-1] = 1
-#           else:
-#               ullman_mat[s, t] = 0
-
+    # Initializing future match table f(u, v) [lines 1 and 2 in Algorithm 2]
+    for u in range(1, g1.number_of_nodes()):
+        for v in range(1, g2.number_of_nodes()):
+            if len(g1.edges(g1.nodes[u])) <= len(g2.edges(g2.nodes[v])):
+                ullman_mat[u - 1, v - 1] = 1
+    #           else:
+    #               ullman_mat[s, t] = 0
     print(ullman_mat)
-    # TODO
+    _ullman_recursive(ullman_mat, g1, g2)
+
     if ullman_mat.all() == 1:
         return True
     return False
